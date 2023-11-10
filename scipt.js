b910209cd0e5e2b48f8646d511a34b76a7ce5b40
Ia janiashvili <iako.janiashvili@gmail.com>
2023-11-10 04:54:17 -0800

    first commit

---
+++ scipt.js
@@ -0,0 +1,42 @@
+let student = null; 
+    student = {
+        userName: 'iako',
+        age: 24,
+        course:'front & development',
+    }
+    console.log(student);
+
+    console.log('my name is' + ' ' + student.userName);
+    console.log('i am' + ' ' + student.age +' ' + 'old'); 
+    console.log(`i am learning  ${student.course} in Smart Academy`);
+
+    // მართხკუთხედის ფართობი
+
+    let rectangle = null;
+        rectangle = {
+            x: 7,
+            y: 5,
+        }
+        console.log(rectangle.x * rectangle.y);
+
+
+    let x = 5;
+    let y = 7;
+    let sum = x*y;
+    console.log(sum);
+
+    //others
+    let z = 15;
+    let b = 3;
+    let sss = z/b;
+    console.log(sss);
+
+    let x2 = 14;
+    let y2 ='12345';
+    let xy2 = x2+y2;
+    console.log(xy2);
+
+    let x3 =3264346;
+    let y3= 37373;
+    let xy3=x3-y3;
+    console.log(xy3);
